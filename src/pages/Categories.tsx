26502f0c746f1d9d2deb1508a47b371d6733daa3
Valeriy <masterbox06@gmail.com>
2023-09-13 18:10:16 +0300

    refactored code, added sorting handle function

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,90 +1,41 @@
-import {Button, Col, Form, Input, Pagination, Row, Select, Switch, Table} from "antd";
+import {Button, Col, Form, Input, Pagination, Row, Select, Space, Switch, Table} from "antd";
 import {useSearchParams} from "react-router-dom";
 import {generateGetUrl, getCategories, updateQueryParams} from "../utils/utils.ts";
 import {useEffect, useMemo, useState} from "react";
 
-const columns = [
-  {
-    value: '_id',
-    label: 'ID',
-    dataIndex: '_id',
-    key: '_id',
-    title: 'ID',
-  },
-  {
-    value: 'code',
-    label: 'Код',
-    dataIndex: 'code',
-    key: 'code',
-    title: 'Код',
-    // sorter: (a: string, b: string) => console.log(a, b),
-  },
-  {
-    value: 'sort',
-    label: 'Sort',
-    dataIndex: 'sort',
-    key: 'sort',
-    title: 'Sort',
-    // sorter: (a: number, b: number) => console.log(a, b),
-  },
-  {
-    value: 'name',
-    label: 'Название',
-    dataIndex: 'name',
-    key: 'name',
-    title: 'Название',
-    // sorter: (a: string, b: string) => console.log(a, b),
-  },
-  {
-    value: 'description',
-    label: 'Описание',
-    dataIndex: 'description',
-    key: 'description',
-    title: 'Описание',
-  },
-  {
-    value: 'active',
-    label: 'Статус',
-    dataIndex: 'active',
-    key: 'active',
-    title: 'Статус',
-    render: (active: boolean) => {
-      return active ? "Активный" : "Неактивный"
-    },
-    // sorter: (a: boolean, b: boolean) => console.log(a, b),
-  },
-  {
-    value: 'answerCount',
-    label: 'Кол-во ответов',
-    dataIndex: 'answerCount',
-    key: 'answerCount',
-    title: 'Кол-во ответов',
-    // sorter: (a: number, b: number) => console.log(a, b),
-  },
-  {
-    value: 'questionCount',
-    label: 'Кол-во вопросов',
-    dataIndex: 'questionCount',
-    key: 'questionCount',
-    title: 'Кол-во вопросов',
-    // sorter: (a) => console.log(a)
-  }
-]
 const activeFilterOptions = [
   {value: 'all', label: 'Все'},
   {value: 'active', label: 'Активные'},
   {value: 'disabled', label: 'Неактивные'},
 ]
 
+type CategoriesType = {
+  _id: string
+  code: string
+  sort: number
+  name: string
+  description: string
+  active: boolean
+  answerCount: number
+  questionCount: number
+}
+
+type DataType = {
+  categories: CategoriesType[]
+  total: number
+}
+
 export const Categories = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const activeFilter = searchParams.get("filter[active]") === null ? "all" : searchParams.get("filter[active]") === "true" ? "active" : "disabled"
-  const page = searchParams.get("page") !== null ? Number(searchParams.get("page")!) : 1
-  const pageSize = searchParams.get("limit") !== null ? Number(searchParams.get("limit")!) : 10
-  const searchName = searchParams.get("filter[name]") !== null ? searchParams.get("filter[name]")! : ""
+  const page = searchParams.get("page") !== null ? Number(searchParams.get("page")) : 1
+  const pageSize = searchParams.get("limit") !== null ? Number(searchParams.get("limit")) : 10
+  const searchName = searchParams.get("filter[name]") !== null ? searchParams.get("filter[name]") : ""
   const advancedSearch = searchParams.get("advancedSearch") !== null ? JSON.parse(searchParams.get("advancedSearch")!) : false
-  const searchCode = searchParams.get("filter[code]") !== null ? searchParams.get("filter[code]")! : ""
+  const searchCode = searchParams.get("filter[code]") !== null ? searchParams.get("filter[code]") : ""
+  const sortBy = searchParams.get("by")
+  const sortOrder = searchParams.get("order") === null ? 1 : Number(searchParams.get("order"))
   let [total, setTotal] = useState(0)
   let [categories, setCategories] = useState([])
   const [loading, setLoading] = useState(false)
@@ -102,7 +53,7 @@ export const Categories = () => {
     updateQueryParams("filter[active]", values.activeFilter === "all" ? null : values.activeFilter === "active" ? "true" : "false", searchParams, setSearchParams)
     updateQueryParams("filter[name]", values.name ? values.name : null, searchParams, setSearchParams)
 
-    if (advancedSearch) return
+    if (!advancedSearch) return
 
     updateQueryParams("filter[code]", values.code ? values.code : null, searchParams, setSearchParams)
   }
@@ -113,11 +64,16 @@ export const Categories = () => {
       updateQueryParams("filter[code]", null, searchParams, setSearchParams)
     }
   }
+  const onSort = (_: any, __:any, sorter: { field: string, order: string | undefined }) => {
+    console.log('sort', sorter.field, "by", sorter.order)
+    updateQueryParams("by", sorter.order === undefined ? null : sorter.field, searchParams, setSearchParams)
+    updateQueryParams("order", sorter.order === undefined ? null : sorter.order === "ascend" ? "1" : "-1", searchParams, setSearchParams)
+  };
 
   useEffect(() => {
     setLoading(true)
     const url = generateGetUrl(window.location.href, "category")
-    console.log("fetching")
+    console.log("fetching", url)
     getCategories(url)
       .then(response => {
         setTotal(response.total)
@@ -125,6 +81,93 @@ export const Categories = () => {
         setLoading(false)
       })
   }, [searchParams]);
+
+  const columns = [
+    {
+      value: '_id',
+      label: 'ID',
+      dataIndex: '_id',
+      key: '_id',
+      title: 'ID',
+    },
+    {
+      value: 'code',
+      label: 'Код',
+      dataIndex: 'code',
+      key: 'code',
+      title: 'Код',
+      sorter: true,
+      defaultSortOrder: sortBy === "code" ? sortOrder === 1 ? "ascend" : "descend" : null,
+    },
+    {
+      value: 'sort',
+      label: 'Sort',
+      dataIndex: 'sort',
+      key: 'sort',
+      title: 'Sort',
+      sorter: true,
+      defaultSortOrder: sortBy === "sort" ? sortOrder === 1 ? "ascend" : "descend" : null,
+    },
+    {
+      value: 'name',
+      label: 'Название',
+      dataIndex: 'name',
+      key: 'name',
+      title: 'Название',
+      sorter: true,
+      defaultSortOrder: sortBy === "name" ? sortOrder === 1 ? "ascend" : "descend" : null,
+    },
+    {
+      value: 'description',
+      label: 'Описание',
+      dataIndex: 'description',
+      key: 'description',
+      title: 'Описание',
+    },
+    {
+      value: 'active',
+      label: 'Статус',
+      dataIndex: 'active',
+      key: 'active',
+      title: 'Статус',
+      render: (active: boolean) => {
+        return active ? "Активный" : "Неактивный"
+      },
+      sorter: true,
+      defaultSortOrder: sortBy === "active" ? sortOrder === 1 ? "ascend" : "descend" : null,
+    },
+    {
+      value: 'answerCount',
+      label: 'Кол-во ответов',
+      dataIndex: 'answerCount',
+      key: 'answerCount',
+      title: 'Кол-во ответов',
+      sorter: true,
+      defaultSortOrder: sortBy === "answerCount" ? sortOrder === 1 ? "ascend" : "descend" : null,
+    },
+    {
+      value: 'questionCount',
+      label: 'Кол-во вопросов',
+      dataIndex: 'questionCount',
+      key: 'questionCount',
+      title: 'Кол-во вопросов',
+      sorter: true,
+      defaultSortOrder: sortBy === "questionCount" ? sortOrder === 1 ? "ascend" : "descend" : null,
+    },
+    {
+      value: 'action',
+      label: 'Действия',
+      dataIndex: 'action',
+      title: 'Действия',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+          <a>Редактировать</a>
+          <a>Удалить</a>
+        </Space>
+      ),
+    },
+  ]
 
   const pagination = useMemo(() => <Pagination
     className="flex justify-end"
@@ -136,8 +179,7 @@ export const Categories = () => {
     onChange={pageChange}
     onShowSizeChange={showSizeChange}
     pageSize={pageSize}
-  />, [page, pageSize, total]);
-
+  />, [page, pageSize, total])
   const search = useMemo(() => <>
     <div className="w-1/2 flex justify-center">
       <Form
@@ -214,8 +256,6 @@ export const Categories = () => {
     <Switch defaultChecked={advancedSearch} onChange={toggleAdvancedSearch}/>
   </>, [advancedSearch, searchName, searchCode, activeFilter, advancedSearch])
 
-
-  console.log(total, categories)
   return (
     <div className="py-8 min-h-screen flex flex-col items-center bg-zinc-200 gap-8">
       {search}
@@ -227,6 +267,8 @@ export const Categories = () => {
         columns={columns}
         pagination={false}
         footer={() => pagination}
+        onChange={onSort}
+
       />
     </div>
   )
