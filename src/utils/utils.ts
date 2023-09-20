7d7ddf4f1261e57c0afe50dee87da422273bd23f
Valeriy <masterbox06@gmail.com>
2023-09-20 16:14:18 +0300

    refactored code, added update function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 import {axiosHeaders, server_name} from "../axiosGlobals.ts";
 import axios from "axios";
+import {CategoryType} from "../pages/Categories.tsx";
 
 export const updateQueryParams = (key: string, value: string | null, searchParams: URLSearchParams, setSearchParams: (searchParams: URLSearchParams) => void) => {
   if (value === null) searchParams.delete(key)
@@ -8,7 +9,7 @@ export const updateQueryParams = (key: string, value: string | null, searchParam
   setSearchParams(searchParams);
 }
 
-export const generateGetUrl = (url: string, type: string) => {
+export const generateGetListUrl = (url: string, type: string) => {
   const urlObj = new URL(url);
   const searchParams = urlObj.searchParams;
 
@@ -25,11 +26,23 @@ export const generateGetUrl = (url: string, type: string) => {
   return `${server_name}/api/${type}?${searchParams.toString()}`;
 }
 
-export const getCategories = async (url: string) => {
+export const getData = async (url: string) => {
   try {
     const response = await axios.get(url, {headers: axiosHeaders})
     return response.data
   } catch (error) {
-    return error
+    // @ts-ignore
+    throw new Error(error.message)
+  }
+}
+
+type UpdateDataType = CategoryType
+
+export const updateData = async (url: string, data: UpdateDataType) => {
+  try {
+    return await axios.post(url, data, {headers: axiosHeaders})
+  } catch (error) {
+    // @ts-ignore
+    throw new Error(error.message)
   }
 }
