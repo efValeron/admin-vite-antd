2884e08f9a1ff9c4b3831a89f530256170fd98bc
Valeriy <masterbox06@gmail.com>
2023-09-21 15:18:41 +0300

    added locale to antd config provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import './index.css'
 import {theme} from "./theme.ts";
 import {ConfigProvider} from "antd";
 import {BrowserRouter} from "react-router-dom";
+import ruRu from 'antd/locale/ru_RU';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <ConfigProvider theme={theme}>
+    <ConfigProvider theme={theme} locale={ruRu}>
       <App/>
     </ConfigProvider>
   </BrowserRouter>,
